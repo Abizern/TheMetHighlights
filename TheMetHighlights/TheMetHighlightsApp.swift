ebe1458d776157684e54a7cb46bcd2c50ebbf16b
Abizer Nasir <abizern@mac.com>
2024-11-06 10:36:39 +0000

    Initial Commit

---
+++ TheMetHighlights/TheMetHighlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMetHighlightsApp.swift
+//  TheMetHighlights
+//
+//  Created by Abizer Nasir on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheMetHighlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheMetHighlightsTests/TheMetHighlightsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheMetHighlightsTests.swift
+//  TheMetHighlightsTests
+//
+//  Created by Abizer Nasir on 06/11/2024.
+//
+
+import Testing
+@testable import TheMetHighlights
+
+struct TheMetHighlightsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
